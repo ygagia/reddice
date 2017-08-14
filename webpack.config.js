fc4026dfbfd8e2d0b607f81d46e7652588cf98e5
John <john@datawave.ro>
2017-08-14 15:08:20 +0300

    Hot reloading setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,28 @@
 import path from 'path';
+import webpack from 'webpack';
 
 export default {
     devtool: 'eval-source-map',
-    entry: path.join(__dirname, '/client/index.js'),
+    entry: [
+        'webpack-hot-middleware/client',
+        path.join(__dirname, '/client/index.js'),
+    ],
     output: {
         filename: 'bundle.js',
-        path: '/'
+        path: '/',
+        publicPath: '/'
     },
+    plugins: [
+        new webpack.NoEmitOnErrorsPlugin(),
+        new webpack.optimize.OccurrenceOrderPlugin(),
+        new webpack.HotModuleReplacementPlugin()
+    ],
     module: {
         loaders: [
             {
                 test: /\.js$/,
                 include: path.join(__dirname, 'client'),
-                loaders: [ 'babel-loader' ]
+                loaders: [ 'react-hot-loader', 'babel-loader' ]
             }
         ]
     },
